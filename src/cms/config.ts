2670106abb66e6ffd973b8592dab49f97a4f109d
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2022-12-28 13:56:38 -0500

    Add NetlifyCMS

--- src/cms/config.ts
+++ src/cms/config.ts
@@ -2,7 +2,7 @@ export default {
   cms_manual_init: true,
   backend: {
     name: 'github',
-    repo: 'jakeprins/serverless-saas',
+    repo: 'brettdoyle44/marcal',
     branch: 'master',
     squash_merges: true,
     // Uncomment these lines and replace the base URL when deploying to Vercel
